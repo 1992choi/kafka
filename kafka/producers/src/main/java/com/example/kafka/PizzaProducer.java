931c27c459fd2c9c4035ddeb9e099e41172e3ab2
choi <younggeunn@naver.com>
2024-04-18 21:05:59 +0900

    Producer의 메시지 배치 전송 내부 메커니즘 - linger.ms와 batch.size 파라미터 고찰

--- kafka/producers/src/main/java/com/example/kafka/PizzaProducer.java
+++ kafka/producers/src/main/java/com/example/kafka/PizzaProducer.java
@@ -94,7 +94,9 @@ public class PizzaProducer {
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.64.2:9092");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.setProperty(ProducerConfig.ACKS_CONFIG, "0");
+        // props.setProperty(ProducerConfig.ACKS_CONFIG, "0");
+        props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "32000");
+        props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
 
         //KafkaProducer object creation
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
