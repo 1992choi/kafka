b9d621b381c8ff9cb6a5e9ff0e9c20075feb6238
choi <younggeunn@naver.com>
2025-06-24 21:31:16 +0900

    [아파치 카프카] 섹션 6. 카프카 프로듀서 애플리케이션 개발

---
+++ apache/src/main/java/kafka/apache/ex01_producer/Ex01_Basic.java
@@ -0,0 +1,37 @@
+package kafka.apache.ex01_producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/*
+    가장 간단한 프로듀서 예제
+ */
+public class Ex01_Basic {
+
+    private final static Logger logger = LoggerFactory.getLogger(Ex01_Basic.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        String messageValue = "testMessage";
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue);
+        producer.send(record);
+        logger.info("{}", record);
+        producer.flush();
+        producer.close();
+    }
+
+}
